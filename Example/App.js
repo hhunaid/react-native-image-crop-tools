96b024034bddc9d80aa396d9600c53e4617fe3e5
Hunaid Hassan <hhunaid@gmail.com>
2020-01-02 05:05:52 +0500

    Added Example

---
+++ Example/App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {useState, useRef} from 'react';
+import {Button, StatusBar, StyleSheet, View, Image} from 'react-native';
+import {CropView} from 'react-native-image-crop-tools';
+import ImagePicker from 'react-native-image-picker';
+
+const App: () => React$Node = () => {
+  const [uri, setUri] = useState();
+  const cropViewRef = useRef();
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <View style={styles.container}>
+        <Button
+          title={'Pick Image'}
+          onPress={() => {
+            ImagePicker.launchImageLibrary({noData: true}, response => {
+              setUri(response.uri);
+            });
+          }}
+        />
+        <CropView
+          sourceUrl={uri}
+          style={styles.cropView}
+          ref={cropViewRef}
+          onImageCrop={(res) => console.warn(res)}
+          keepAspectRatio
+          aspectRatio={16.0 / 9.0}
+        />
+        <Button
+          title={'Get Cropped View'}
+          onPress={() => {
+            cropViewRef.current.saveImage(90);
+          }}
+        />
+      </View>
+    </>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  cropView: {
+    flex: 1,
+    backgroundColor: 'red'
+  },
+});
+
+export default App;
---
+++ Example/ios/Dummy.swift
@@ -0,0 +1,9 @@
+//
+//  Dummy.swift
+//  ImageCropToolsExample
+//
+//  Created by Hunaid Hassan on 31/12/2019.
+//  Copyright © 2019 Facebook. All rights reserved.
+//
+
+import Foundation
